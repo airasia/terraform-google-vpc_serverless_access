f0f1e4cec8dadebb7ed0836715c9ec77bc8229c0
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-10-14 02:20:40 +0800

    Re-arrange lines to bring non-default variable declarations on top

--- variables.tf
+++ variables.tf
@@ -7,26 +7,26 @@ variable "name_suffix" {
   }
 }
 
+variable "vpc_name" {
+  description = "Name of the VPC Network for which this Serverless VPC Access connector will be created."
+  type        = string
+}
+
+variable "ip_cidr_range" {
+  description = "A non-overlapping /28 IP CIDR range that is unused by the VPC Netowrk elsewhere. The VPC Connector will create connector instances on IP addresses in this range."
+  type        = string
+}
+
 variable "connector_name" {
   description = "An arbitrary name for the Serverless VPC Access connector."
   type        = string
   default     = "vpc-connector"
 }
 
-variable "vpc_name" {
-  description = "Name of the VPC Network for which this Serverless VPC Access connector will be created."
-  type        = string
-}
-
 variable "region" {
   description = "Region where the Serverless VPC Access connector resides. Defaults to the Google provider's region if nothing is specified here. See https://cloud.google.com/compute/docs/regions-zones"
   type        = string
   default     = ""
-}
-
-variable "ip_cidr_range" {
-  description = "A non-overlapping /28 IP CIDR range that is unused by the VPC Netowrk elsewhere. The VPC Connector will create connector instances on IP addresses in this range."
-  type        = string
 }
 
 variable "connector_timeout" {
