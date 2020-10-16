b8db9b869caa9e41baba3b5343d7474268991ebe
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-10-16 22:54:23 +0800

    Bugfix: enable servicenetworking.googleapis.com API as prerequisite

--- main.tf
+++ main.tf
@@ -7,6 +7,11 @@ locals {
   connector_region = var.region != "" ? var.region : data.google_client_config.google_client.region
 }
 
+resource "google_project_service" "networking_api" {
+  service            = "servicenetworking.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_project_service" "serverless_vpc_api" {
   service            = "vpcaccess.googleapis.com"
   disable_on_destroy = false
@@ -17,7 +22,7 @@ resource "google_vpc_access_connector" "vpc_connector" {
   region        = local.connector_region
   network       = var.vpc_name
   ip_cidr_range = var.ip_cidr_range
-  depends_on    = [google_project_service.serverless_vpc_api]
+  depends_on    = [google_project_service.networking_api, google_project_service.serverless_vpc_api]
   timeouts {
     create = var.connector_timeout
     delete = var.connector_timeout
